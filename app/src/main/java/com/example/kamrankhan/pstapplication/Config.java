2b8d4f16be209c45f73c870cc4119cc1a969bc7f
kamrankhan180190 <68084134+kamrankhan180190@users.noreply.github.com>
2020-07-15 01:59:10 +0500

    Update Config.java

--- app/src/main/java/com/example/kamrankhan/pstapplication/Config.java
+++ app/src/main/java/com/example/kamrankhan/pstapplication/Config.java
@@ -4,11 +4,8 @@ package com.example.kamrankhan.pstapplication;
  * Created by Kamran Khan on 11/21/2016.
  */
 public class Config {
-    public static final String EMAIL ="kamideegreat@gmail.com";
-    //public static final String EMAIL ="kamrankhanstar@yahoo.com";
-    public static final String PASSWORD ="aspire5710";
+    public static final String EMAIL ="your@email.com";
+    public static final String PASSWORD ="yourPassword";
 
-    //public static final String EMAIL ="kamrankhanstar@hotmail.com";
-    //public static final String PASSWORD ="Aspire5710";
 }
 
